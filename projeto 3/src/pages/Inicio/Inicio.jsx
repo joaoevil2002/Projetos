0785de11bd925cf08ab81fc8f45d466f44963a61
joaoevil2002 <145061436+joaoevil2002@users.noreply.github.com>
2023-11-24 13:35:53 -0300

    Add files via upload

---
+++ projeto 3/src/pages/Inicio/Inicio.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Pizza from './todosprodutos/pizza'
+import Promocão from './todosprodutos/promo';
+import Hamburguer from './todosprodutos/hamburguer'
+import Esfirra from './todosprodutos/esfirra'
+import Pastel from './todosprodutos/pastel'
+
+function Inicio() {
+  return (
+    <>
+    <div><Promocão/></div>
+    {/*filtro* */}
+  
+    <div><Pizza/></div>
+    <div><Hamburguer/></div>
+    <div><Esfirra/></div>
+    <div><Pastel/></div>
+  
+         
+   
+    </>
+  );
+}
+
+export default Inicio;
---
+++ projeto 3/src/pages/Inicio/todosprodutos/pizza.jsx
@@ -0,0 +1,49 @@
+import './produtos.css'
+import { useEffect, useState } from "react";
+
+function Pizza() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/static/static/pizza.json")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  return (
+    <div className="container">
+    <div className='search'>
+       
+       <p>pasteis</p> 
+    
+   </div>
+      <div className="carousel">
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name} />
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">{oldPrice}</span>
+                <span className="price">{price}</span>
+               <button className='button'>+ adicionar ao carrinho</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+     
+     
+
+
+     
+
+
+    </div>
+  );
+}
+
+export default Pizza;
