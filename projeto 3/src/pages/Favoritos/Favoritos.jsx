3eb2619ea2987f1c01b6ae7f8df50a56416e5766
joaoevil2002 <145061436+joaoevil2002@users.noreply.github.com>
2023-12-04 16:51:04 -0300

    Add files via upload

---
+++ projeto 3/src/pages/Favoritos/Favoritos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Favoritos = () => {
+  return (
+    <div>
+      <h1>Favoritos</h1>
+    </div>
+  );
+};
+
+export default Favoritos;
---
+++ projeto 3/src/pages/Inicio/todosprodutos/pizza.jsx
@@ -0,0 +1,77 @@
+import './produtos.css';
+import { useEffect, useState } from 'react';
+import { FaHeart, FaStar } from 'react-icons/fa';
+
+function Pizza() {
+  const [data, setData] = useState([]);
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [tooltipText, setTooltipText] = useState('');
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const handleButtonClick = (event, productName) => {
+    const rect = event.target.getBoundingClientRect();
+    const x = rect.right + 10; // Posicionamento à direita do ícone
+    const y = rect.top - 25; // Ajuste para alinhamento com o topo do ícone
+    setTooltipPosition({ x, y });
+    setTooltipText(productName);
+    setTooltipVisible(true);
+
+    setTimeout(() => {
+      setTooltipVisible(false);
+      setTooltipText('');
+    }, 2000); // Fecha o tooltip após 2 segundos (você pode ajustar o tempo conforme necessário)
+  };
+
+  useEffect(() => {
+    fetch('/static/static/pizza.json') // Atualizei o caminho do arquivo JSON
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="search">
+        <p>pizza</p>
+      </div>
+      <div className="carousel">
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image-container">
+                <img src={image} alt={name} loading="lazy" />
+                <div
+                  className="heart-icon"
+                  onClick={(event) => handleButtonClick(event, name)}
+                >
+                  <FaHeart style={{ verticalAlign: 'middle' }} />
+                </div>
+                <div className="fa-star">
+                  <FaStar />
+                </div>
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">{oldPrice}</span>
+                <span className="price">{price}</span>
+                <button className="button">+ adicionar ao carrinho</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* Tooltip */}
+      {tooltipVisible && (
+        <div
+          className="tooltip"
+          style={{ top: tooltipPosition.y, left: tooltipPosition.x }}
+        >
+          {tooltipText}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Pizza;
